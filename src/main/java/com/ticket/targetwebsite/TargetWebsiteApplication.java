69f8f3b9344a80d83918ea68ccb469e3d7ee8b05
hyungkishin <tkaqkeldk@nate.com>
2024-03-03 23:32:54 +0900

    chore: initial commit

---
+++ src/main/java/com/ticket/targetwebsite/TargetWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.targetwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetWebsiteApplication.class, args);
+    }
+
+}
